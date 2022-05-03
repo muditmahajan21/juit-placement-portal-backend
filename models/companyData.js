81b22d3e1aab139cbc3856e194382c02aa43ab1a
Mudit Mahajan <muditmahajan21@gmail.com>
2022-05-03 16:52:14 +0530

    added student review form

--- models/companyData.js
+++ models/companyData.js
@@ -1,40 +1,106 @@
 const mongoose = require('mongoose')
 
-const questionSchema =  new mongoose.Schema({
-    question: {
+const jobSchema = new mongoose.Schema({
+    jobTitle: {
         type: String,
-        required: true
+       
     },
-    round: {
+    package: {
         type: String,
-        required: true
+    },
+    jobDescription: {
+        type: String,
     }
 })
 
-questionSchema.set('toJSON', {
+jobSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
+        delete returnedObject.__v
     }
 })
 
-const companyDataSchema = new mongoose.Schema({
+companyDataSchema = new mongoose.Schema({
     companyName: {
         type: String,
         required: true,
     },
-    questions: [questionSchema],
-    year: {
+    onlineTestVenue: {
         type: String,
-        required: true
     },
-    hired: {
+    totalInterviewRound: {
         type: String,
-        required: true
     },
-    package: {
+    techInterviewVenus: {
         type: String,
-        required: true
+    },
+    piInterviewVenus: {
+        type: String,
+    },
+    totalQuestions: {
+        type: String,
+    },
+    totalTechnicalQuestions: {
+        type: String,
+    },
+    totalAptitudeQuestions: {
+        type: String,
+    },
+    totalDuration: {
+        type: String,
+    },
+    jobs: [jobSchema],
+    aptitudeRound1: {
+        type: Array,
+    },
+    technicalRound1: {
+        type: Array,
+    },
+    technicalRound2: {
+        type: Array,
+    },
+    technicalRound3: {
+        type: Array,
+    },
+    technicalRound4: {
+        type: Array,
+    },
+    technicalRound5: {
+        type: Array,
+    },
+    technicalRound6: {
+        type: Array,
+    },
+    hrRound2: {
+        type: Array,
+    },
+    hrRound3: {
+        type: Array,
+    },
+    hrRound4: {
+        type: Array,
+    },
+    hrRound5: {
+        type: Array,
+    },
+    hrRound6: {
+        type: Array,
+    },
+    gdRound2: {
+        type: Array,
+    },
+    gdRound3: {
+        type: Array,
+    },
+    gdRound4: {
+        type: Array,
+    },
+    gdRound5: {
+        type: Array,
+    },
+    gdRound6: {
+        type: Array,
     },
 })
 
